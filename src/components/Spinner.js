a52f58baee094cdfada7f97af089b5cdaec25db3
James Pickering <james.pickering@gmail.com>
2021-09-12 19:35:46 +0100

    update spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import '../styles/spinner.css';
-
-function init(max) {
-  const arr = [];
-  for (let i = 0; i < max; i++) {
-    arr.push(<div key={i}></div>);
-  }
-  return arr;
-}
+import React from "react";
+import "../styles/spinner.css";
 
 export default function Spinner() {
-  return (
-    <div className='spinner'>
-      {init(12)}
-    </div>
-  );
+	return (
+		<div className="spinner">
+			<svg
+				width="40px"
+				height="40px"
+				viewBox="0 0 50 50"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					fill="#000"
+					d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z"
+				>
+					<animateTransform
+						attributeType="xml"
+						attributeName="transform"
+						type="rotate"
+						from="0 25 25"
+						to="360 25 25"
+						dur="0.6s"
+						repeatCount="indefinite"
+					/>
+				</path>
+			</svg>
+		</div>
+	);
 }
